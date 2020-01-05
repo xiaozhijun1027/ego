f99d0386251cab3d2176102cfa158f9a08c824ba
darth-xiaozhijun <15717061273@163.com>
2020-01-05 20:38:24 +0800

    图片上传

--- ego-manager/src/main/webapp/WEB-INF/js/common.js
+++ ego-manager/src/main/webapp/WEB-INF/js/common.js
@@ -25,7 +25,7 @@ var TT = EGO = {
 		//指定上传文件参数名称
 		filePostName  : "uploadFile",
 		//指定上传文件请求的url。
-		uploadJson : '/pic/upload',
+		uploadJson : '/ego-manager/pic/upload',
 		//上传类型，分别为image、flash、media、file
 		dir : "image"
 	},
